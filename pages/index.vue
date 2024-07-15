e4fbdc6bc02c89b8a43d6704f7bdc627644f0390
Vincent Flebus <vflebus@gmail.com>
2024-07-15 23:25:01 +0200

    mobile map zoom

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-screen h-screen bg-seaBlue overflow-x-hidden">
     <Transition v-if="!animationsStore.animations.introPlayed" name="fade" mode="in-out">
-      <div v-if="showIntro === 'gif'" class="fixed top-0 left-0 w-screen h-screen">
+      <div v-if="showIntro === 'gif'" class="fixed top-0 left-0 w-screen h-screen z-50">
         <img :src="home" alt="" class="w-full h-full object-cover object-center" />
         <button
           class="absolute top-[70%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 max-w-[340px] aspect-[3/1] z-10 border-8 border-brown rounded-full bg-beige font-sneakers-500 text-brown text-[2.5rem]"
@@ -10,46 +10,51 @@
           ENTER
         </button>
       </div>
-      <div v-else-if="showIntro === 'video'" class="fixed top-0 left-0 w-screen h-screen">
+      <div v-else-if="showIntro === 'video'" class="fixed top-0 left-0 w-screen h-screen z-50">
         <video preload="true" autoplay class="w-full h-full object-cover object-center opacity-100 transition-all duration-1000" @play="fadeTimeOut" @ended="introEnded" ref="videoPlayer">
           <source :src="introVideo" type="video/webm" />
         </video>
       </div>
     </Transition>
     <div class="w-screen h-full flex flex-col lg:flex-row justify-start items-center">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1279 810" class="shrink-0 w-screen max-h-screen lg:w-[75%]">
-        <image width="1279" height="810" class="h-full w-auto shadow-[0_0_32px_32px_#384E9F_inset]" :xlink:href="map"></image>
-        <rect x="0" y="0" fill="transparent" width="388" height="88" class="cursor-pointer" @click="currentRegionKey = '0'"></rect>
-        <circle cx="207" cy="535" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '29'"></circle>
-        <circle cx="443" cy="325" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '7'"></circle>
-        <circle cx="458" cy="199" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '6'"></circle>
-        <circle cx="452" cy="110" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '25'"></circle>
-        <circle cx="476" cy="110" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '28'"></circle>
-        <circle cx="616" cy="125" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '22'"></circle>
-        <circle cx="496" cy="255" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '2'"></circle>
-        <circle cx="584" cy="235" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '21'"></circle>
-        <circle cx="666" cy="214" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '23'"></circle>
-        <circle cx="761" cy="99" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '24'"></circle>
-        <circle cx="753" cy="144" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '27'"></circle>
-        <circle cx="453" cy="422" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '3'"></circle>
-        <circle cx="524" cy="610" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '4'"></circle>
-        <circle cx="531" cy="741" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '1'"></circle>
-        <circle cx="590" cy="690" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '8'"></circle>
-        <circle cx="648" cy="542" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '10'"></circle>
-        <circle cx="695" cy="426" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '11'"></circle>
-        <circle cx="618" cy="418" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '12'"></circle>
-        <circle cx="577" cy="480" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '9'"></circle>
-        <circle cx="916" cy="117" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '26'"></circle>
-        <circle cx="852" cy="228" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '17'"></circle>
-        <circle cx="821" cy="342" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '20'"></circle>
-        <circle cx="916" cy="372" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '18'"></circle>
-        <circle cx="1073" cy="490" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '16'"></circle>
-        <circle cx="1104" cy="342" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '19'"></circle>
-        <circle cx="870" cy="526" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '15'"></circle>
-        <circle cx="794" cy="542" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '14'"></circle>
-        <circle cx="872" cy="656" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '13'"></circle>
-      </svg>
-      <div class="w-[90%] lg:h-full lg:w-[25%] bg-[#384E9F] shadow-[0_0_32px_32px_#384E9F] lg:pr-8 py-8" ref="regionsInfosDiv">
+      <div class="shrink-0 w-screen max-h-screen lg:w-[75%] relative">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1279 810" class="w-[100%] h-[100%] !cursor-default" v-panzoom="panZoomOptions">
+          <image width="1279" height="810" class="h-full w-auto shadow-[0_0_32px_32px_#384E9F_inset]" :xlink:href="map"></image>
+          <rect x="0" y="0" fill="transparent" width="388" height="88" class="cursor-pointer" @click="currentRegionKey = '0'"></rect>
+          <circle cx="207" cy="535" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '29'"></circle>
+          <circle cx="443" cy="325" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '7'"></circle>
+          <circle cx="458" cy="199" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '6'"></circle>
+          <circle cx="452" cy="110" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '25'"></circle>
+          <circle cx="476" cy="110" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '28'"></circle>
+          <circle cx="616" cy="125" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '22'"></circle>
+          <circle cx="496" cy="255" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '2'"></circle>
+          <circle cx="584" cy="235" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '21'"></circle>
+          <circle cx="666" cy="214" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '23'"></circle>
+          <circle cx="761" cy="99" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '24'"></circle>
+          <circle cx="753" cy="144" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '27'"></circle>
+          <circle cx="453" cy="422" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '3'"></circle>
+          <circle cx="524" cy="610" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '4'"></circle>
+          <circle cx="531" cy="741" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '1'"></circle>
+          <circle cx="590" cy="690" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '8'"></circle>
+          <circle cx="648" cy="542" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '10'"></circle>
+          <circle cx="695" cy="426" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '11'"></circle>
+          <circle cx="618" cy="418" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '12'"></circle>
+          <circle cx="577" cy="480" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '9'"></circle>
+          <circle cx="916" cy="117" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '26'"></circle>
+          <circle cx="852" cy="228" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '17'"></circle>
+          <circle cx="821" cy="342" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '20'"></circle>
+          <circle cx="916" cy="372" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '18'"></circle>
+          <circle cx="1073" cy="490" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '16'"></circle>
+          <circle cx="1104" cy="342" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '19'"></circle>
+          <circle cx="870" cy="526" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '15'"></circle>
+          <circle cx="794" cy="542" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '14'"></circle>
+          <circle cx="872" cy="656" r="10" fill="transparent" class="cursor-pointer" @click="currentRegionKey = '13'"></circle>
+        </svg>
+        <div v-if="!zoomedOnce" class="absolute top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center lg:hidden" @pointerdown="zoomedOnce = true">
+          <Icon name="material-symbols:pan-zoom-rounded" class="text-white animate-pulse" size="4rem" />
+        </div>
+      </div>
+      <div class="w-full lg:h-full lg:w-[25%] bg-seaBlue shadow-[0_0_32px_32px_#384E9F] lg:pr-8 py-8 px-[5%] relative z-10" ref="regionsInfosDiv">
         <div class="w-full h-full border-8 border-brown rounded-3xl bg-beige p-4 text-center flex flex-col justify-start items-center">
           <h1 class="text-gold font-sneakers-500 text-[2.5rem] break-words">{{ currentRegion.title }}</h1>
           <div class="w-full h-[2px] bg-brown rounded-full my-4"></div>
@@ -59,7 +64,7 @@
           </p>
           <NuxtLink
             :to="`/region/${currentRegionKey}`"
-            class="aspect-[4/1] w-1/2 lg:w-full border-8 border-brown rounded-full bg-gold font-sneakers-500 text-brown lg:text-[2.5rem] flex justify-center items-center mt-8"
+            class="aspect-[4/1] w-1/2 lg:w-full border-8 border-brown rounded-full bg-gold font-sneakers-500 text-brown lg:text-[2rem] flex justify-center items-center mt-8"
             @click="showIntro = 'video'"
           >
             LEARN MORE
@@ -96,14 +101,21 @@ const currentRegionKey = ref<keyof typeof regionsData>("0");
 const currentRegion = computed<Region>(() => regionsData[currentRegionKey.value]);
 
 const regionsInfosDiv = ref<HTMLDivElement>();
-watch(currentRegionKey, () => {
-  regionsInfosDiv.value?.scrollIntoView({ behavior: "smooth" });
-});
 
 const introEnded = () => {
   showIntro.value = false;
   animationsStore.animations.introPlayed = true;
 };
+
+const panZoomOptions = {
+  contain: "outside",
+  step: 1,
+  maxScale: 6,
+  panOnlyWhenZoomed: true,
+  handleStartEvent: () => {},
+};
+
+const zoomedOnce = ref(false);
 </script>
 
 <style scoped>
--- pages/region/[id].vue
+++ pages/region/[id].vue
@@ -1,5 +1,8 @@
 <template>
   <div class="w-full min-h-screen bg-gold flex flex-col justify-start items-center">
+    <NuxtLink to="/" class="fixed top-4 left-4 z-10">
+      <Icon name="material-symbols:arrow-back-rounded" class="text-white" size="10vh" />
+    </NuxtLink>
     <header class="w-full h-screen relative">
       <img src="~/assets/images/regions/shrooms.png" alt="" class="w-full h-full object-cover object-center" />
       <h1 class="absolute bottom-0 w-full text-[4rem] font-sneakers-500 text-white text-center bg-gold shadow-[0_-32px_32px_32px_#DAA052]">{{ currentRegion.title }}</h1>
@@ -18,9 +21,9 @@
 
       <div class="w-full flex flex-col items-center">
         <h2 class="text-[4vh] font-sneakers-500 text-white text-center">DISCOVER THE CHARACTERS</h2>
-        <div class="flex flex-col gap-0 w-2/3 items-start">
+        <div class="flex flex-col w-2/3 items-start">
           <div v-for="character in currentRegion.characters" class="w-full lg:w-2/3 flex flex-col lg:flex-row items-center lg:even:self-end lg:even:flex-row-reverse">
-            <img :src="character.image" alt="" class="w-full lg:w-auto lg:h-[300px]" />
+            <img :src="character.image" alt="" class="w-full lg:w-auto lg:h-[400px]" />
             <div class="text-justify lg:text-start">
               <h3 class="font-sneakers-500 text-[2rem] text-center lg:text-start">{{ character.name }}</h3>
               <p>{{ character.description }}</p>
