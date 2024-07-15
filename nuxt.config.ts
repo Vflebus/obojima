a9caa3a6e838f26cb42931324fcf1a148c503759
Vincent Flebus <vflebus@gmail.com>
2024-07-15 23:52:06 +0200

    remove nuxt/panzoom lib from modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@wgr-sa/nuxt-panzoom', "@nuxt/icon"],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', "@nuxt/icon"],
   pinia: {
     storesDirs: ['./stores/**'],
   },
