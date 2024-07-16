c8ebe3030883bfcdda39e2161d92f5831502dc82
Vincent Flebus <vflebus@gmail.com>
2024-07-16 17:02:01 +0200

    font size reponsive region title

--- pages/region/[id].vue
+++ pages/region/[id].vue
@@ -5,7 +5,9 @@
     </NuxtLink>
     <header class="w-full h-screen relative">
       <img :src="currentRegion.image" alt="" class="w-full h-full object-cover object-center" ref="headerImg" />
-      <h1 class="absolute bottom-0 w-full text-[4rem] font-sneakers-500 text-white text-center bg-seaBlue shadow-[0_-32px_32px_32px_#384e9f]" ref="mainTitle">{{ currentRegion.title }}</h1>
+      <h1 class="absolute bottom-0 w-full text-[2rem] xl:text-[4rem] font-sneakers-500 text-white text-center bg-seaBlue shadow-[0_-32px_32px_32px_#384e9f]" ref="mainTitle">
+        {{ currentRegion.title }}
+      </h1>
     </header>
     <main class="w-full p-8 flex flex-col items-center text-white gap-8">
       <div class="w-full lg:w-1/3 flex flex-col gap-4 text-justify">
@@ -20,7 +22,7 @@
       </div>
 
       <div class="w-full flex flex-col items-center">
-        <h2 class="text-[4vh] font-sneakers-500 text-white text-center lg:mb-32">DISCOVER THE CHARACTERS</h2>
+        <h2 class="text-[2rem] font-sneakers-500 text-white text-center lg:mb-32">DISCOVER THE CHARACTERS</h2>
         <div class="flex flex-col w-2/3 items-start gap-4 lg:gap-0">
           <div v-for="character in currentRegion.characters" class="w-full lg:w-2/3 flex flex-col lg:flex-row items-center lg:even:self-end lg:even:flex-row-reverse lg:-mt-32">
             <img :src="character.image" alt="" class="w-full lg:w-auto lg:h-[400px]" />
@@ -66,7 +68,8 @@ const setBackgroundColor = async () => {
   }
   const colorThief = new ColorThief();
   let color;
-  if (headerImg.value?.complete) {
+  if (!headerImg.value) return;
+  if (headerImg.value.complete) {
     color = await colorThief.getColor(headerImg.value);
     console.log(color);
     if (color) {
@@ -75,8 +78,8 @@ const setBackgroundColor = async () => {
       mainTitle.value!.style.boxShadow = `0 -32px 32px 32px rgb(${color[0]}, ${color[1]}, ${color[2]})`;
     }
   } else {
-    headerImg.value?.addEventListener("load", async function () {
-      color = await colorThief.getColor(headerImg.value);
+    headerImg.value.addEventListener("load", async function () {
+      color = await colorThief.getColor(headerImg.value!);
       console.log(color);
       if (color) {
         containerDiv.value!.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
